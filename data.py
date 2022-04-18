99b7b0b1636bba86c4a89e68987865354bc2c3e2
Ethan Jordon <72542756+rejordon93@users.noreply.github.com>
2022-04-18 16:05:14 -0700

    Add files via upload

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores repellat assumenda excepturi at hic harum voluptate qui, ullam, molestiae error sed! Dolorum provident aspernatur molestias molestiae reprehenderit voluptatem oriosam harum.',
+            'author': 'Ethan Jordon',
+            'create_date': '04-18-2020'
+        },
+         {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. harum voluptate qui, ullam, molestiae error sed! Dolorum provident aspernatur molestias molestiae reprehenderit voluptatem oriosam harum.',
+            'author': 'John Doe',
+            'create_date': '06-8-2012'
+        },
+          {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores repellat assumenda excepturi at hic harum voluptate qui, ullam, molestiae error sed! Dolorum provident aspernatur.',
+            'author': 'Noah Smith',
+            'create_date': '11-23-2017'
+        }
+    ]
+    
+    return articles
